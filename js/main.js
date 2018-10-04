3241cbc42c7258d58de1e5f41d4416d093ad3be2
pharzan <farzan.tinati@gmail.com>
2018-10-04 13:44:17 +0330

    added skelton css and styles

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 import Test from "./components/test.js";
 import m from "./lib/mithril/mithril.js";
+import "../css/styles.css";
 
-const mount_point = document.getElementById("mount");
+const mount_point = document.getElementById("main");
 
 m.mount(mount_point, Test);
 
